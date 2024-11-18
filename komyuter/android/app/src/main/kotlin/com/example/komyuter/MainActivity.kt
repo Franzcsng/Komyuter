9b5ad7b42d17f1e6bc269b91aaef615f71cf151a
Franzcsng <129406053+Franzcsng@users.noreply.github.com>
2024-11-19 03:57:28 +0800

    Delete komyuter directory

--- komyuter/android/app/src/main/kotlin/com/example/komyuter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.komyuter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- komyuter/lib/colors/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color mainBlue = Color.fromARGB(255, 35, 118, 213);
-  static const Color darkBlueAccent = Color.fromARGB(255, 21, 94, 177);  // Green
-  static const Color danger = Color(0xFFF44336);  // Red
-  static const Color warning = Color(0xFFFFC107);  // Yellow
-  static const Color success = Color(0xFF4CAF50);  // Green
-}
